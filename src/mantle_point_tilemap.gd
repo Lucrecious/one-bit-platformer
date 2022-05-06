a2cbcd11b81f2bb7a2ac5e963b541085e5ac431b
Lucrecious <luca.duran@hotmail.com>
2022-05-05 20:52:05 -0400

    Add level editor

--- src/mantle_point_tilemap.gd
+++ src/mantle_point_tilemap.gd
@@ -1,4 +1,7 @@
 extends TileMap
 
-export(int, LAYERS_2D_PHYSICS) var mantle_collision_layer := 0
-export(int) var mantle_size := 5.0
+export(int, LAYERS_2D_PHYSICS) var mantle_collision := 0
+
+func _ready():
+	var rect := get_used_rect()
+	TileMapGenerator.add_mantle_points(self, rect, 2, mantle_collision)
--- src/sync_viewports.gd
+++ src/sync_viewports.gd
@@ -9,25 +9,6 @@ onready var _canvas_layer := NodE.get_node_with_error(self, _canvas_layer_path, 
 onready var _viewport := NodE.get_node_with_error(self, _viewport_path, Viewport) as Viewport
 onready var _sprite := get_node(_sprite_path)
 
-func _input(event: InputEvent) -> void:
-	var mouse_button := event as InputEventMouseButton
-	if not mouse_button:
-		return
-	
-	if not mouse_button.pressed:
-		return
-	
-	if mouse_button.button_index != BUTTON_LEFT:
-		return
-	
-	var players := get_tree().get_nodes_in_group('player')
-	if players.empty():
-		return
-	
-	var player := players[0] as KinematicBody2D
-	
-	player.global_position = get_global_mouse_position()
-
 func _ready() -> void:
 	_canvas_layer.custom_viewport = _viewport
 
