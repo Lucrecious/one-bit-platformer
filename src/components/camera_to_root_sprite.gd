9eb321ab445fc51efc48fd2954298e27ef7da650
Lucrecious <luca.duran@hotmail.com>
2022-05-06 20:28:01 -0400

    Edit level with in-game level editor

--- src/components/camera_to_root_sprite.gd
+++ src/components/camera_to_root_sprite.gd
@@ -5,4 +5,5 @@ func _ready() -> void:
 	yield(get_tree(), 'idle_frame')
 	var camera := NodE.get_sibling_with_error(self, Camera2D) as Camera2D
 	camera.get_parent().remove_child(camera)
+	camera.current = true
 	Components.root_sprite(get_parent()).add_child(camera)
