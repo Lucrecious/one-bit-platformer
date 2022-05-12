feaa3c977e04afef09f1e030517088b37a9f0bfa
Lucrecious <luca.duran@hotmail.com>
2022-05-12 00:34:18 -0400

    Add checkpoint and tilemap rect manipulator plugin

---
+++ src/actors/checkpoint.gd
@@ -0,0 +1,13 @@
+extends AnimatedSprite
+
+signal checkpointed()
+
+
+onready var _area := $Area as Area2D
+
+func _ready() -> void:
+	_area.connect('body_entered', self, '_on_body_entered', [], CONNECT_ONESHOT)
+
+func _on_body_entered(_body) -> void:
+	play('waving')
+	emit_signal('checkpointed')
--- src/mantle_point_tilemap.gd
+++ src/mantle_point_tilemap.gd
@@ -4,6 +4,7 @@ export(int, LAYERS_2D_PHYSICS) var mantle_collision := 0
 
 func _ready():
 	var rect := get_used_rect()
+	printt(get_path(), rect)
 	TileMapGenerator.add_mantle_points(self, rect, 2, mantle_collision)
 	
 	LevelEditor.commands.connect('tilemap_changed', self, '_on_tilemap_changed')
